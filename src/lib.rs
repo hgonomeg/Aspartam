0079d586950bdc01e16a0363438d3f77d3408c4f
hgonomeg <hgonomeg@gmail.com>
2022-09-07 04:03:06 +0200

    Draft implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,118 @@
 use async_trait::async_trait;
-use std::sync::{Arc, Weak};
-use tokio::sync::Mutex;
+use std::{sync::{Arc, Weak}, marker::PhantomData};
+use tokio::{sync::Mutex,sync::mpsc};
+use futures_util::{stream,sink};
 pub struct ActorContext<T: Actor> {
     address: WeakAddr<T>,
+    msg_queue: MessageQueue<T>
+}
+
+trait EnvelopeProxy<A: Actor>{
+
+}
+struct Envelope<T>(Box<dyn EnvelopeProxy<T> + Send>);
+
+
+impl<T> Envelope<T> {
+    // fn new() -> Self {
+        
+    // }
+}
+
+struct MessageQueue<T: Actor> {
+    tx: mpsc::UnboundedSender<Envelope<T>>
+}
+
+impl<T: Actor> MessageQueue<T> {
+    fn new() -> (Self,mpsc::UnboundedReceiver<Envelope<T>>) {
+        let (tx,rx) = mpsc::unbounded_channel();
+        (Self {
+            tx
+        },rx)
+    }
+    async fn send<M>(&self, msg: M) where T: Handler<M>, M: Send {
+
+    }
 }
 
 impl<T: Actor> ActorContext<T> {
     pub fn address(&self) -> Addr<T> {
         self.address.upgrade().unwrap()
     }
-}
-
-#[derive(Clone)]
-pub struct Addr<T> {
-    _inner: Arc<Mutex<T>>,
-}
-
-#[derive(Clone)]
-pub struct WeakAddr<T> {
-    _inner: Weak<Mutex<T>>,
-}
-
-impl<T> WeakAddr<T> {
-    pub fn upgrade(&self) -> Option<Addr<T>> {
-        self._inner.upgrade().map(|x| Addr::<T> { _inner: x })
+    fn empty(msg_queue: MessageQueue<T>) -> Self {
+        Self {
+            address: WeakAddr::<T>::empty(),
+            msg_queue
+        }
+    }
+    fn set_weakaddr(&mut self, source: WeakAddr<T>) {
+        self.address = source;
     }
 }
 
-impl<T> Addr<T> {
-    pub async fn send(msg: T) {}
-    pub fn downgrade(&self) -> WeakAddr<T> {
-        WeakAddr::<T> {
-            _inner: Arc::<tokio::sync::Mutex<T>>::downgrade(&self._inner),
+#[derive(Clone)]
+pub struct Addr<T: Actor> {
+    _inner: Arc<Mutex<T>>,
+    ctx: Arc<Mutex<ActorContext<T>>>
+}
+
+#[derive(Clone)]
+pub struct WeakAddr<T: Actor> {
+    _inner: Weak<Mutex<T>>,
+    ctx: Weak<Mutex<ActorContext<T>>>
+}
+
+impl<T: Actor> WeakAddr<T> {
+    pub fn upgrade(&self) -> Option<Addr<T>> {
+        Some(
+            Addr::<T> {
+                _inner: self._inner.upgrade()?,
+                ctx: self.ctx.upgrade()?
+            }
+        )
+    }
+    fn empty() -> Self {
+        Self {
+            _inner: Weak::new(),
+            ctx: Weak::new()
         }
     }
 }
 
-pub trait Actor: Sized {
-    
+impl<T: Actor> Addr<T> {
+    pub async fn send<M>(&self, msg: M) where M: Send, T: Handler<M> {
+       let ctx_lock = self.ctx.lock().await; 
+       ctx_lock.msg_queue.send(msg).await;
+    }
+    pub fn downgrade(&self) -> WeakAddr<T> {
+        WeakAddr::<T> {
+            _inner: Arc::<Mutex<T>>::downgrade(&self._inner),
+            ctx: Arc::<Mutex<ActorContext<T>>>::downgrade(&self.ctx)
+        }
+    }
+}
+
+#[async_trait]
+pub trait Actor: 'static + Sized {
+    async fn start(self) -> Addr<Self> {
+        let (msg_queue,mut msg_rx) = MessageQueue::new();
+        
+        tokio::spawn(async move { 
+            while let Some(msg) = msg_rx.recv().await {
+
+            }
+        });
+
+        let ret = Addr::<Self> { 
+            _inner: Arc::from(Mutex::from(self)),
+            ctx: Arc::from(Mutex::from(ActorContext::empty(msg_queue)))
+        };
+        let mut ctx_lock = ret.ctx.lock().await;
+        ctx_lock.set_weakaddr(ret.downgrade());
+        drop(ctx_lock);
+        ret
+    }
+    async fn started();
 }
 
 #[async_trait]
