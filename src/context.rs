ee7a98ed644ea4045a68f75fbfda95031c3dc256
hgonomeg <hgonomeg@gmail.com>
2022-09-13 03:02:55 +0200

    Add ActorContext::notify()

--- src/context.rs
+++ src/context.rs
@@ -10,9 +10,19 @@ pub struct ActorContext<T: Actor> {
 unsafe impl<T: Actor> Send for ActorContext<T> {}
 
 impl<T: Actor> ActorContext<T> {
+    #[inline]
     pub fn address(&self) -> Addr<T> {
         self.address.upgrade().unwrap()
     }
+    #[inline]
+    pub fn notify<M>(&self, msg: M) 
+    where
+        M: 'static + Send,
+        T: Handler<M>
+    {
+        self.address().do_send(msg)
+    }
+    #[inline]
     pub fn weak_address(&self) -> WeakAddr<T> {
         self.address.clone()
     }
