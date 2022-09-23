b2da75abb1a4044a1e437f0078de31234a432c6e
hgonomeg <hgonomeg@gmail.com>
2022-09-23 19:55:13 +0200

    Implement running supervised actors

--- src/runner.rs
+++ src/runner.rs
@@ -18,20 +18,18 @@ async fn stopping_check<A: Actor>(act: &mut A, ctx: &mut ActorContext<A>) {
     }
 }
 
-pub(crate) async fn supervised_actor_runner_loop<A: Supervised>(
-    _act: A,
-    _ctx: ActorContext<A>,
-    _msg_rx: UnboundedReceiver<QueuePayload<A>>,
-) {
-    unimplemented!("todo: implement supervised_actor_runner_loop");
-    // actor_runner_loop(act, ctx, msg_rx).await
+struct FinishedActor<A: Actor> {
+    actor: A,
+    ctx: ActorContext<A>,
+    died_from_dropping_last_reference: bool,
+    msg_rx: UnboundedReceiver<QueuePayload<A>>
 }
 
-pub(crate) async fn actor_runner_loop<A: Actor>(
+async fn actor_runner_loop_impl<A: Actor>(
     mut act: A,
     mut ctx: ActorContext<A>,
     mut msg_rx: UnboundedReceiver<QueuePayload<A>>,
-) {
+) -> FinishedActor<A> {
     // starting phase
     assert_eq!(ctx.state(), ActorState::Starting);
     act.started(&mut ctx).await;
@@ -39,6 +37,7 @@ pub(crate) async fn actor_runner_loop<A: Actor>(
         ctx.set_state(ActorState::Running);
     }
     stopping_check(&mut act, &mut ctx).await;
+    let mut died_from_dropping_last_reference = false;
     if ctx.state() != ActorState::Stopped {
         loop {
             let mut _fresh_addr_opt = None;
@@ -58,6 +57,7 @@ pub(crate) async fn actor_runner_loop<A: Actor>(
                     });
                     ctx.reset_from(_fresh_addr_opt.as_ref().unwrap().downgrade());
                     msg_rx = new_rx;
+                    died_from_dropping_last_reference = true;
                 }
                 Some(mut msg) => {
                     msg.handle(&mut act, &mut ctx).await;
@@ -69,11 +69,50 @@ pub(crate) async fn actor_runner_loop<A: Actor>(
             //     2. calling ctx.stop() in Handler<M>::handle()
             stopping_check(&mut act, &mut ctx).await;
             if ctx.state() == ActorState::Stopped {
+                // just exit, leaving everything as is
                 break;
+            } else {
+                // the actor might have just recovered from the Stopping state.
+                // We need to clear the boolean flag.
+                died_from_dropping_last_reference = false;
             }
         }
     }
     // final phase
     assert_eq!(ctx.state(), ActorState::Stopped);
     act.stopped(&mut ctx).await;
+    FinishedActor {
+        actor: act,
+        ctx,
+        died_from_dropping_last_reference,
+        msg_rx
+    }
+}
+
+/// Should be very similar to [actor_runner_loop] except that the actor gets restarted when it's Stopped.
+/// 
+/// The actor might actually die when all references to it are dropped.
+pub(crate) async fn supervised_actor_runner_loop<A: Supervised>(
+    mut act: A,
+    mut ctx: ActorContext<A>,
+    mut msg_rx: UnboundedReceiver<QueuePayload<A>>,
+) {
+    loop {
+        let finished_actor = actor_runner_loop_impl(act, ctx, msg_rx).await;
+        if finished_actor.died_from_dropping_last_reference {
+            break;
+        } else {
+            act = finished_actor.actor;
+            ctx = finished_actor.ctx;
+            msg_rx = finished_actor.msg_rx;
+        }
+    }
+}
+
+pub(crate) async fn actor_runner_loop<A: Actor>(
+    act: A,
+    ctx: ActorContext<A>,
+    msg_rx: UnboundedReceiver<QueuePayload<A>>,
+) {
+    let _ = actor_runner_loop_impl(act, ctx, msg_rx).await;
 }
